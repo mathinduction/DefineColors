6cd42ab2cde269e091648213db4c532779ed638d
Мария <matindukciya@rambler.ru>
2013-09-30 23:53:32 +0700

    Сделан метод одиночной связи
    Сделан сам алгоритм, остался вывод на экран цветов и задание eps

--- DefineColors/Methods/IMethod.cs
+++ DefineColors/Methods/IMethod.cs
@@ -8,7 +8,42 @@ namespace DefineColors.Methods
 {
 	public abstract class IMethod
 	{
+		protected List<Color> _pixels = new List<Color>();
+		protected List<List<Color>> _clusters = new List<List<Color>>(); 
+
 		public abstract string MethodName();
-		public abstract List<Color> FindColors(Bitmap bitmap);
+		public virtual List<Color> FindColors(Bitmap bitmap)
+		{
+			_pixels = BitmapToList(bitmap);
+			return null;
+		}
+
+		/// <summary>
+		/// "Расстояние" между цветами
+		/// </summary>
+		protected double ColorDistance(Color color1, Color color2)
+		{
+			double rez =
+				Math.Sqrt((color1.R - color2.R)*(color1.R - color2.R) +
+				          (color1.G - color2.G)*(color1.G - color2.G) +
+				          (color1.B - color2.B)*(color1.B - color2.B));
+
+			return rez;
+		}
+
+		private List<Color> BitmapToList(Bitmap bitmap)
+		{
+			if (bitmap == null)
+				return null;
+
+			for (int i = 0; i < bitmap.Width; i++)
+				for (int j = 0; j < bitmap.Height; j++)
+				{
+					_pixels.Add(bitmap.GetPixel(i,j));
+				}
+
+			return _pixels;//TODO убрать повторы
+		}
 	}
 }
+
--- DefineColors/Methods/MethodOneLink.cs
+++ DefineColors/Methods/MethodOneLink.cs
@@ -8,6 +8,8 @@ namespace DefineColors.Methods
 {
 	public class MethodOneLink : IMethod
 	{
+		private double _eps = 100;//TODO задание из интерфейса + нормировку на sqrt(195075) = sqrt(255*255 + 255*255 + 255*255)
+
 		public override string MethodName()
 		{
 			return "Метод одиночной связи";
@@ -15,8 +17,60 @@ namespace DefineColors.Methods
 
 		public override List<Color> FindColors(Bitmap bitmap)
 		{
+			base.FindColors(bitmap);
+	
 			List<Color> colors = new List<Color>();
+			List<Color> cluster = new List<Color>();
+
+			while (_pixels.Count() != 0)
+			{
+				int i = 0; 
+				int newColorsInCluster = 0;
+				cluster = new List<Color>();
+				cluster.Add(_pixels[0]);
+				_pixels.RemoveAt(0);
+				int size = _pixels.Count();
+				#region //Повторять до тех пор, пока есть цвета, достаточно близкие хотя бы к одному цвету в кластере
+				do
+				{
+					newColorsInCluster = 0;
+					while (i < size)//До тех пор пока есть непросмотренные цвета
+					{
+						if (AddPredicate(_pixels[i], cluster))//Добавляем цвет
+						{
+							cluster.Add(_pixels[i]);
+							_pixels.RemoveAt(i);
+							size--;
+							newColorsInCluster++;
+						}
+						else
+						{
+							i++;
+						}
+					}
+					
+				} while (newColorsInCluster > 0);
+				_clusters.Add(cluster);
+				#endregion
+			}
+			
 			return colors;
+		}
+
+		/// <summary>
+		/// Добавлять ли указанный цвет в кластер
+		/// </summary>
+		private bool AddPredicate(Color color, List<Color> cluster)
+		{
+			double d = _eps + 1;
+			int i = -1;
+			while (d > _eps && i < cluster.Count() - 1)
+			{
+				i++;
+				d = ColorDistance(color, cluster[i]);
+			}
+
+			return d <= _eps;
 		}
 	}
 }
